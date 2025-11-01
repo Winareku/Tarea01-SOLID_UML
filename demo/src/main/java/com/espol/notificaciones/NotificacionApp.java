c9050ac06081778e4e89c191101ae879e67eec89
Etienne Defranc <156627201+EtienneDAka@users.noreply.github.com>
2025-11-01 10:21:15 -0500

    feat: Notificaciones

---
+++ demo/src/main/java/com/espol/notificaciones/NotificacionApp.java
@@ -0,0 +1,7 @@
+package com.espol.notificaciones;
+
+import com.espol.interfaces.INotificacion;
+
+public class NotificacionApp implements INotificacion {
+    public void enviar(String mensaje, String destinatario) {}
+}
---
+++ demo/src/main/java/com/espol/notificaciones/NotificacionSMS.java
@@ -0,0 +1,7 @@
+package com.espol.notificaciones;
+
+import com.espol.interfaces.INotificacion;
+
+public class NotificacionSMS implements INotificacion {
+    public void enviar(String mensaje, String destinatario) {}
+}
